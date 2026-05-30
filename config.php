cbf0da7ce7b71a4c1a0a335a16a574c1f58aca49
Nouhaila Aitari <nohaaitari2005@gmail.com>
2026-05-30 21:20:44 +0200

    feat(database): configurer connexion base de donnees

--- config.php
+++ config.php
@@ -1,17 +1,15 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'rayhana_db';
-$username = 'root';
-$password = '';
-
+$host = "sql310.infinityfree.com";
+$dbname = 'if0_42056372_u315291019_rayhana_db';
+$username = 'admin';
+$password = '123admin';
 try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8",
         $username,
         $password
     );
-
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
