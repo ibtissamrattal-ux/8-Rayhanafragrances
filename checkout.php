345e9b66025ddb6e1be7a1b9e4de4581d0e2b40b
nouhailaaitari <nohaaitari2005@gmail.com>
2026-06-01 00:18:08 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-
+// Vérifier si l'utilisateur est connecté
 if(!isset($_SESSION['user_id'])){
-
+ // Redirection vers la page de connexion
     header("Location: login.php");
     exit();
 
